75e80539a128512edbaed88a3cd4540c33e1c0fc
T-LES <lucaselat@gmail.com>
2024-10-13 23:26:16 +0100

    update views

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -56,32 +56,32 @@
             <form id="loginForm" action="{{route('login')}}" method="POST">
                 @csrf
                 <div class="input-group mb-3">
-                    <input type="email" name="email" id="email" value="{{ old('email') }}"  autofocus class="form-control @error('email') is-invalid @enderror" placeholder="Votre adresse email">
+                    <input type="email" name="email"  value="{{ old('email') }}"  autofocus class="form-control @error('email') is-invalid @enderror" placeholder="Votre adresse email">
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-envelope"></span>
                         </div>
                     </div>
                     @error('email')
-                    <span id="email_error" class="invalid-feedback" role="alert">
+                    <span  class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
                     @enderror
-                    <span id="email_error" class="invalid-feedback" role="alert"></span>
+                    <span  class="invalid-feedback" role="alert"></span>
                 </div>
                 <div class="input-group mb-3">
-                    <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror" placeholder="Votre mot de passe">
+                    <input type="password" name="password"  class="form-control @error('password') is-invalid @enderror" placeholder="Votre mot de passe">
                     <div class="input-group-append">
                         <div class="input-group-text">
                             <span class="fas fa-lock"></span>
                         </div>
                     </div>
                     @error('password')
-                    <span id="password_error" class="invalid-feedback" role="alert">
+                    <span  class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
                     @enderror
-                    <span id="password_error" class="invalid-feedback" role="alert"></span>
+                    <span  class="invalid-feedback" role="alert"></span>
                 </div>
                 <div class="row mb-3">
                     <div class="col-8">
